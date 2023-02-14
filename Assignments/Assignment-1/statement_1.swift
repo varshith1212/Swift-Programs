de3b0965ed4bb49c30f16199814440ceb0c23d26
varshith <doddasaivarshith1212@gmail.com>
2023-02-14 14:52:02 +0530

    Added Question to file

--- Assignments/Assignment-1/statement_1.swift
+++ Assignments/Assignment-1/statement_1.swift
@@ -1,3 +1,7 @@
+/*
+Create a function that accepts a string as an input. If the string ends in "-s," then the string should return TRUE to indicate that the word is plural. Otherwise, it should return FALSE. Keep in mind that the function should return a Boolean value (TRUE or FALSE) and not a string value ("true" or "false").
+*/
+
 func plural(word: String) -> Bool{
     if String(word.suffix(2)) == "-s"{
         return true
--- Assignments/Assignment-1/statement_2.swift
+++ Assignments/Assignment-1/statement_2.swift
@@ -1,3 +1,7 @@
+/*
+Create a function that takes an input string and reverses its order and also switches capitalization so that each new word is capitalized. For example, "I love India!" should return "!Aidni Evol I."
+*/
+
 import Foundation
 
 func reverseString(sentence: String) -> String{
