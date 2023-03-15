04a47c90e04cbc3f3b213e5bb70007ba0cc738d8
Varshith <doddasaivarshith1212@gmail.com>
2023-03-15 16:38:35 +0530

    Validation added

--- LoginPage/LoginPage/LoginViewController.swift
+++ LoginPage/LoginPage/LoginViewController.swift
@@ -13,15 +13,21 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var password: UITextField!
     
+    var masterUserName: String = "Admin"
+    var masterPassword: String = "admin123"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        userName.text?.removeAll()
+        password.text?.removeAll()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -30,10 +36,30 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func onClickLogin(_ sender: Any) {
-        let registerViewController = self.storyboard?.instantiateViewController(identifier: "RegisterViewControllerID") as! RegisterViewController
-        registerViewController.userNameText = userName.text
-        registerViewController.passwordText = password.text
-        self.navigationController?.pushViewController(registerViewController, animated: true)
+        if userName.text == "" || password.text == "" {
+            showAlertWithTitle("Enter credentials")
+            userName.text?.removeAll()
+            password.text?.removeAll()
+        } else if userName.text != masterUserName {
+            showAlertWithTitle("UserName Not Matching")
+            userName.text?.removeAll()
+        } else if password.text != masterPassword {
+            showAlertWithTitle("Password Not Matching")
+            password.text?.removeAll()
+        }else {
+            let registerViewController = self.storyboard?.instantiateViewController(identifier: "RegisterViewControllerID") as! RegisterViewController
+            registerViewController.userNameText = userName.text
+            registerViewController.passwordText = password.text
+            self.navigationController?.pushViewController(registerViewController, animated: true)
+        }
+        
+    }
+    
+    func showAlertWithTitle(_ title: String, message: String? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: false)
     }
     
 }
